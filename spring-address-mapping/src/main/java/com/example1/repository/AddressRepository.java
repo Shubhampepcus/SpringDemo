b83c83758d2b7af8ef815cf4a614c6c600623dc1
shubham_pepcus <102805401+Shubhampepcus@users.noreply.github.com>
2022-05-16 18:02:03 +0530

    Delete spring-address-mapping directory

--- spring-address-mapping/src/main/java/com/example1/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example1.repository;
-
-import com.example1.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Integer> {
-}
--- spring-address-mapping/src/main/java/com/example1/repository/EmployeeCustemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example1.repository;
-
-import com.example1.entity.Employee;
-
-import java.util.List;
-
-public interface EmployeeCustemRepository {
-  List<Employee> findByDepartment(String name, String department);
-}
